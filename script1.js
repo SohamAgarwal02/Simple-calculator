da36a96a1093f94e7c6c518f9fb4706e361eb9fe
Soham Agarwal <agarwal.soham106@gmail.com>
2026-06-05 12:39:56 +0530

    Initial calculator project

---
+++ script1.js
@@ -0,0 +1,27 @@
+function calculate(operation) {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let result;
+
+    if (operation === "add") {
+        result = num1 + num2;
+    }
+    else if (operation === "subtract") {
+        result = num1 - num2;
+    }
+    else if (operation === "multiply") {
+        result = num1 * num2;
+    }
+    else if (operation === "divide") {
+
+        if (num2 === 0) {
+            result = "Cannot divide by zero";
+        } else {
+            result = num1 / num2;
+        }
+    }
+
+    document.getElementById("result").innerText =
+        "Result: " + result;
+}
